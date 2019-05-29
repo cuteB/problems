5a962751647521b788b53e9fdfbd52851eeb43fd
Brandon Godley <brandongodley2@gmail.com>
2019-05-29 15:49:53 -0600

    buy sell stocks

--- buySellStocks.js
+++ buySellStocks.js
@@ -1,12 +1,36 @@
 
 
 let buySellStocks = (arr) => {
-    return 5;
+    let buy = 0,
+        sell = 0,
+        amount = 0,
+        n = arr.length;
+
+    //        v
+    // [7,1,5,3,6,4]
+
+    if (n <= 1) {
+        return 0;
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (arr[i] < arr[buy]) {
+            buy = i;
+            if (sell < i) {
+                sell = i;
+            }
+        } else if (arr[i] > arr[sell]) {
+            sell = i;
+            if (amount < (arr[sell] - arr[buy])) {
+                amount = arr[sell] - arr[buy];
+            }
+        }
+    }
+
+
+    return amount;
 }
 
-let t = () => {
-    return 1;
-}
 
 module.exports = {
     buySellStocks
--- test/testBuySellStocks.js
+++ test/testBuySellStocks.js
@@ -6,9 +6,49 @@ const assert = require('chai').assert;
 
 describe('Test Buy Sell stocks', function() {
     it('basic', function() {
-        let arr = [7, 1, 5, 3, 6, 4]
+        let arr = [7, 1, 5, 3, 6, 4];
         let act = buySellStocks(arr);
-        let exp = 5
+        let exp = 5;
+
+        assert.equal(act, exp);
+    })
+
+    it('increasing', function() {
+        let arr = [1,2,3,4,5,6,7,8,9,10];
+        let act = buySellStocks(arr);
+        let exp = 9;
+
+        assert.equal(act, exp);
+    })
+
+    it('decreasing', function() {
+        let arr = [10,9,8,7,6,5,4,3,2,1];
+        let act = buySellStocks(arr);
+        let exp = 0;
+
+        assert.equal(act, exp);
+    })
+
+    it('ups and downs', function() {
+        let arr = [2,6,8,6,5,1,4];
+        let act = buySellStocks(arr);
+        let exp = 6;
+
+        assert.equal(act, exp);
+    })
+
+    it('ups and downs 2', function() {
+        let arr = [2,6,8,6,5,1,10];
+        let act = buySellStocks(arr);
+        let exp = 9;
+
+        assert.equal(act, exp);
+    })
+
+    it('last increase', function() {
+        let arr = [9,8,7,6,5,4,5];
+        let act = buySellStocks(arr);
+        let exp = 1
 
         assert.equal(act, exp);
     })
