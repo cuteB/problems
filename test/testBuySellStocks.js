3aad476f76e90d5233e606636389b90db1e619d5
Fuben <brandongodley2@gmail.com>
2019-08-19 16:53:49 -0600

    buy sell stocks

--- test/testBuySellStocks.js
+++ test/testBuySellStocks.js
@@ -1,4 +1,4 @@
-let {buySellStocks} = require('../buySellStocks.js');
+let {buySellStocks} = require('../arrays/buySellStocks.js');
 
 let test = require('unit.js');
 
@@ -6,9 +6,41 @@ const assert = require('chai').assert;
 
 describe('Test Buy Sell stocks', function() {
     it('basic', function() {
-        let arr = [7, 1, 5, 3, 6, 4]
+        let arr = [7, 1, 5, 3, 6, 4];
         let act = buySellStocks(arr);
-        let exp = 5
+        let exp = 5;
+
+        assert.equal(act, exp);
+    })
+
+    it('short', function() {
+        let arr = [1,2];
+        let act = buySellStocks(arr);
+        let exp = 1;
+
+        assert.equal(act, exp);
+    })
+
+    it('one', function() {
+        let arr = [1];
+        let act = buySellStocks(arr);
+        let exp = 0;
+
+        assert.equal(act, exp);
+    })
+
+    it('null', function() {
+        let arr = null;
+        let act = buySellStocks(arr);
+        let exp = null;
+
+        assert.equal(act, exp);
+    })
+
+    it('long', function() {
+        let arr = [65,32,12,14,5,15,23,65,4,44,21,32,56,18,111];
+        let act = buySellStocks(arr);
+        let exp = 107;
 
         assert.equal(act, exp);
     })
